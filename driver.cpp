7f98b1f815211183fc0540194c94ffe7d440171b
Lesley-Amezcua <lesleygamezcua@student.hartnell.edu>
2015-10-09 11:59:05 -0700

    Create driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,46 @@
+//Lesley Amezcua
+//Athena Raya
+//Louis Romero
+//Dario Molina
+#include<iostream>
+#include<cstring>
+#include<cctype>
+#include<cassert>
+using namespace std;
+
+char menu( );
+double climbing();
+double scuba();
+double skyDive();
+double spelunk();
+
+int main()
+{
+
+  return 0;
+}
+char menu()
+{
+
+
+}
+double climbing()
+{
+
+
+}
+double scuba()
+{
+
+
+}
+double skyDive()
+{
+
+
+}
+double spelunk()
+{
+
+
+}
